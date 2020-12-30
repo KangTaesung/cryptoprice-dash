05737f80baa65d39d03d0461fc74a3b90348bf91
kangtaesung <congoha@gmail.com>
2020-12-30 18:27:46 +0900

    get currencies from coinbase

--- src/components/cryptoprice-dash.js
+++ src/components/cryptoprice-dash.js
@@ -1,8 +1,51 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element';
+import {afterNextRender} from '@polymer/polymer/lib/utils/render-status';
+import '@polymer/iron-ajax/iron-ajax.js';
 
 export class CryptopriceDash extends PolymerElement {
     static get is() {
         return 'cryptoprice-dash';
+    }
+
+    static get properties() {
+        return {
+            currencies: {
+                type: Array,
+                value: [
+                    {
+                        code: 'BTC',
+                        name: 'Bitcoin'
+                    },
+                    {
+                        code: 'ETH',
+                        name: 'Ethereum'
+                    },
+                    {
+                        code: 'LTC',
+                        name: 'Litecoin'
+                    }
+                ]
+            }
+        }
+    }
+
+    ready() {
+        super.ready();
+        afterNextRender(this, function() {
+            this.getCurrencyData(this.currencies);
+        });
+    }
+
+    getCurrencyData(currencies) {
+        let ajax = this.$.coinbase;
+        currencies.forEach(element => {
+            ajax.url = 'https://api.coinbase.com/v2/prices/' + element.code + '-USD/spot';
+            ajax.generateRequest();
+        });
+    }
+
+    handleResponse(response) {
+        console.log(response);
     }
 
     constructor() {
@@ -12,6 +55,8 @@ export class CryptopriceDash extends PolymerElement {
     static get template() {
         return html`
         <h2>Hello world!</h2>
+        <iron-ajax id="coinbase" handle-as="json" on-response="handleResponse">
+        </iron-ajax>
         `;
     }
 }
